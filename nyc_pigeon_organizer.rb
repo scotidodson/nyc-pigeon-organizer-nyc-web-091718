179ddd04d7eae4e1ab9c58a88f82f27cb5b32d7d
Scoti Dodson <scotidodson@gmail.com>
2018-09-12 21:55:33 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -21,7 +21,7 @@ pigeon_data = {
 
 def nyc_pigeon_organizer(pigeon_data)
   pigeon_data.each do |color, color_hash|
-      hash_two.each do |attribute, pigeons|
+      color_hash.each do |attribute, pigeons|
         puts attribute
         binding.pry
         pigeons.each do |name|
