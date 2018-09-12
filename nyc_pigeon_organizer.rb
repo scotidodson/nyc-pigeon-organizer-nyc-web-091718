b48cb9b7100df22dde1eada9bcfe46e0b14d897a
Scoti Dodson <scotidodson@gmail.com>
2018-09-12 21:41:17 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -20,13 +20,21 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(pigeon_data)
-  pigeon_data.each_with_object({}) { |
+  pigeon_data.each_with_object({}) do |a, b| 
+    puts a
 
-  puts organized_pigeons
   binding.pry
+  end 
 end
 
-%w(foo bar).each_with_object({}) { |str, hsh| hsh[str] = str.upcase }
-# => {'foo' => 'FOO', 'bar' => 'BAR'}
+#%w(foo bar).each_with_object({}) { |str, hsh| hsh[str] = str.upcase }
+## => {'foo' => 'FOO', 'bar' => 'BAR'}
 
 nyc_pigeon_organizer(pigeon_data)
+
+
+
+
+
+
+
